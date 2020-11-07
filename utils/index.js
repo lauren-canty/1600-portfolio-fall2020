1397880788e0c4a649cf0368bf0d02c1daaa1c95
Lauren Canty <laurenpaige2000@gmail.com>
2020-11-07 11:23:37 -0700

    modals addeed to starships

--- utils/index.js
+++ utils/index.js
@@ -1,5 +1,14 @@
 export function removeChildren(container) {
-    while (container.firstChild);
-    container.removeChild(container,firstChild)
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
 
+export function getLastNumber(url) {
+    let end = url.lastIndexOf('/')
+    let start = end -2
+    if (url.CharAt(start)=== '/') {
+        start++
+    }
+    return url.slice(start, end)
 }
