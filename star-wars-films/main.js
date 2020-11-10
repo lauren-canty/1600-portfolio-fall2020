dce2147afc49d17419e7b2a0d72776105f9561ff
Lauren Canty <laurenpaige2000@gmail.com>
2020-11-09 21:24:58 -0700

    films js

--- star-wars-films/main.js
+++ star-wars-films/main.js
@@ -1,32 +1,32 @@
 import {films } from '../data/films.js'
-import {people} from '..data/people.js'
+import {people} from '../data/people.js'
 
-//console.log(people.length)
-//console.log(films[0])
-
+const main = document.querySelector('main')
 
 
-/*people.forEach(fperson => {
-    let newParagraph = document.body.appendChild(document.createElement('p'))
-    newParagraph.textContent = person.name
-    
-})*/
+
 for(let step = 0; step<7; step++) {
-for (const film of films) {
     let figure = document.createElement('figure')
-    let figImg = document.createElement('img')
-    let figcaption = document.createElement('figcaption')
-    newImg.src =  `https://starwars-visualguide.com/#/`
-    figcaption.textContent = films[step]
+    let newImg = document.createElement('img')
+    newImg.src = `https://starwars-visualguide.com/assets/img/films/${step + 1}.jpg`
     
-    figure.appendChild
-    
-    
-
-    main.appendChild(newimg)
-}}
-
-
-for(let step = 0; step<7; step++){
+    main.appendChild(newImg)
     console.log(films[step].title);
 }
+    
+
+
+
+/*for (const film of films) {
+    let newImg = document.createElement('img')
+    newImg.src = 'https://starwars-visualguide.com/assets/img/films/2.jpg'
+    
+    main.appendChild(newImg)
+    console.log(film.title);
+}
+
+films.forEach(film=> {
+    let newParagraph = document.body.appendChild(document.createElement('p'))
+    newParagraph.textContent = film.name
+})*/
+
